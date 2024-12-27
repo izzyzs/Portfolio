902cf331fb7c71ee71e9d1fcff5464bbb24a4943
izzyzs <izsoum2024@gmail.com>
2024-12-27 11:11:51 -0500

    complete - except for contact form; working on that now

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="row-start-3 flex gap-6 items-center justify-center py-10 px-3 lg:px-40 bg-[#767676]">
+            <img src="svg/linkedin.svg" alt="" className=" text-gray-300" />
+            <img src="svg/x.svg" alt="" />
+            <img src="svg/youtube.svg" alt="" />
+        </footer>
+    );
+};
+
+export default Footer;
